8ba5b22c44a0618d6c5dd364de257646cc3bf10a
JunSeong_Park <ttotto0608@naver.com>
2025-12-15 17:09:45 +0900

    README 작성

---
+++ README.md
@@ -0,0 +1,12 @@
+# 물리학 기반의 계산기
+
+## api 가이드
+1. speed/
+```
+1. 1차원 좌표계에서 위치 변화에 따른 속도 계산
+request
+- distance : double(m)
+- time : double(s)
+response
+- speed : double(m/s)
+```
---
+++ src/main/java/com/inho/phyculator/PhyculatorApplication.java
@@ -0,0 +1,13 @@
+package com.inho.phyculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhyculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhyculatorApplication.class, args);
+	}
+
+}
