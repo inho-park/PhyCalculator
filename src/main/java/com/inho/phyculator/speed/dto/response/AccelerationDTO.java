5605ab42e88d3ea060d5ef6494e2e1c1483d3d0c
JunSeong_Park <ttotto0608@naver.com>
2025-12-18 16:39:17 +0900

    feat : 가속도 계산 response DTO 생성

---
+++ src/main/java/com/inho/phyculator/speed/dto/response/AccelerationDTO.java
@@ -0,0 +1,14 @@
+package com.inho.phyculator.speed.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccelerationDTO {
+    private double acceleration;
+}
