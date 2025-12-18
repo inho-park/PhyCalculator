61299ab00b16a6f7c3387ded2bfc2efcf66fc067
JunSeong_Park <ttotto0608@naver.com>
2025-12-18 16:38:26 +0900

    refactor : 시간 유효성 검사 함수 이름 변경
    - timeValidCheck

--- src/main/java/com/inho/phyculator/speed/service/SpeedService.java
+++ src/main/java/com/inho/phyculator/speed/service/SpeedService.java
@@ -2,6 +2,7 @@ package com.inho.phyculator.speed.service;
 
 import static com.inho.phyculator.speed.message.ErrorMessage.ARITHMETIC_TIME_ERROR_MESSAGE;
 
+import com.inho.phyculator.speed.dto.response.AccelerationDTO;
 import com.inho.phyculator.speed.dto.response.SpeedDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -12,16 +13,16 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class SpeedService {
     public SpeedDTO getSpeed(double distance, double time) {
-        speedValidCheck(time);
+        timeValidCheck(time);
         return SpeedDTO.builder().speed(distance / time).build();
     }
 
     public SpeedDTO getSpeed(double start, double rear, double time) {
-        speedValidCheck(time);
+        timeValidCheck(time);
         return SpeedDTO.builder().speed((rear - start) / time).build();
     }
 
-    public void speedValidCheck(double time) {
+    public void timeValidCheck(double time) {
         if (time <= 0d) {
             throw new ArithmeticException(ARITHMETIC_TIME_ERROR_MESSAGE.getMessage());
         }
